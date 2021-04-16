68035aa9c1afbd13eea3452fb0672857f3fe98ff
sunyton <sunytoncxw@gmail.com>
2021-04-16 17:15:29 +0800

    测试2

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 
+//% color="#6699ff" weight=25 icon="\uf1d4"
 namespace Test_Arducam {
 
 
     //% blockId=arducam_LED1 block="LED light at pin %pin"
+    //% weight=5
+    //% blockGap=8
+    //% color="#C814B8"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=1
     export function LED1(pin: DigitalPin) {
 
     }
